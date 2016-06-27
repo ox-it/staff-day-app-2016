d69a43238134629c65f36546edf33dc8d5ceb284
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-06-27 17:06:34 +0100

    FoundView1: show continue button when angle > 170deg

--- app/views/FoundView1.js
+++ app/views/FoundView1.js
@@ -9,11 +9,29 @@ define([
 	var Found1View = Backbone.View.extend({
 
 		template: found1Template,
+		
+		THRESHOLD_ANGLE: 170,
 
 		initialize: function(params) {
+			if (window.DeviceOrientationEvent) {
+				$(window).on('deviceorientation', this, _.bind(this.deviceOrientationHandler, this));
+			}
+		},
 		
+		afterRender: function () {
+			$("#continue-button-1").hide();
+		},
+		
+		deviceOrientationHandler: function (ev) {
+			if (ev.originalEvent.beta > this.THRESHOLD_ANGLE) {
+				$("#continue-button-1").show();
+			}
 		},
 
+		cleanup: function () {
+			$(window).off('deviceorientation');
+		},
+		
 	});
 
 	return Found1View;
