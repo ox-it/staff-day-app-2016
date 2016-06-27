8cb1115b699b9dee8d9c1f7bd0654112decf446e
Markos Ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2016-06-27 18:36:16 +0100

    FoundView3: blow into the microphone

--- app/views/FoundView3.js
+++ app/views/FoundView3.js
@@ -2,16 +2,37 @@ define([
 			'backbone', 
 			'hbs!app/templates/found3'
 		], function(
-			Backbone, 
+			Backbone,
 			found3Template
 ) {
 
 	var Found3View = Backbone.View.extend({
-
+		
 		template: found3Template,
 
-		initialize: function(params) {
+		MIC_THRESHOLD: -8,
 		
+		initialize: function(params) {
+			MicrophoneLevels.start();
+			window.addEventListener('microphonelevels', _.bind(this.onMicLevels, this));
+		},
+		
+		afterRender: function() {
+			$("#final-message").hide();
+		},
+
+		onMicLevels: function(data) {
+			if (data.averagePower > this.MIC_THRESHOLD) {
+				this.onStartBlow();
+			}
+		},
+
+		onStartBlow: function() {
+			$("#final-message").show();
+		},
+
+		cleanup: function () {
+			$(window).removeEventListener('microphonelevels', this.onMicLevels);
 		},
 
 	});
